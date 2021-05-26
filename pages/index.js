acd82ad33bf1a020af0335de711c08dc72094d33
dustin <kadeniasone@web.de>
2021-05-26 20:23:30 +0200

    first styling and image

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,38 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
+      <nav className="menu" tabindex="0">
+        <div className="smartphone-menu-trigger"></div>
+        <header className="avatar">
+          <h2>Portfolio</h2>
+        </header>
+        <ul>
+          <li tabindex="0" class="icon-dashboard"><span>Dashboard</span></li>
+          <li tabindex="0" class="icon-customers"><span>Customers</span></li>
+          <li tabindex="0" class="icon-users"><span>Users</span></li>
+          <li tabindex="0" class="icon-settings"><span>Settings</span></li>
+        </ul>
+      </nav>
+
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Image
+          src="/image1.png"
+          width={500}
+          height={300}
+          alt="there should be an image" />
       </main>
 
-      <Footer />
     </div>
   )
 }
